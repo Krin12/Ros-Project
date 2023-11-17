72ddeb612792f2da1c61cb1ce999317e3a4d8912
ros2 <ros2@test.com>
2023-11-17 12:15:06 +0900

    colcon build

--- ros2_term_project/build/ros2_term_project/share/ros2_term_project/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/ros2/AutoCar/Ros-Project/ros2_term_project/build/ros2_term_project"
--- ros2_term_project/install/ros2_term_project/share/ros2_term_project/package.sh
+++ ros2_term_project/install/ros2_term_project/share/ros2_term_project/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/ros2_term_project/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/ros2_term_project/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/ros2_term_project/share/ros2_term_project/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
